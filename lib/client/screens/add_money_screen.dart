975225321de8bf490bdf8d1b167fc3457324b082
AppDeveloperKishan <97960546+KishanD24@users.noreply.github.com>
2025-05-21 23:51:05 -0500

    - astrologer list dynamic data update - add money screen added

---
+++ lib/client/screens/add_money_screen.dart
@@ -0,0 +1,164 @@
+import 'package:astrology_app/common/utils/colors.dart';
+import 'package:astrology_app/common/utils/images.dart';
+import 'package:flutter/material.dart';
+
+class AddMoneyScreen extends StatelessWidget {
+  final List<AmountOption> options = [
+    AmountOption(25, ),
+    AmountOption(50, ),
+    AmountOption(100, isPopular: true),
+    AmountOption(200, ),
+    AmountOption(250, ),
+    AmountOption(500, ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryDark2,
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text("Add money to wallet",style: TextStyle(color: AppColors.textWhite),),
+        leading: BackButton(color: Colors.white,),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.ic_user_dashboard_background),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black54, // Adjust opacity (0-255)
+              BlendMode.darken,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: kToolbarHeight+16,),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Available Balance",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 4),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child:
+                Text(
+                  "₹ 0",
+                  style: TextStyle(fontSize: 24,color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: GridView.count(
+                  padding: EdgeInsets.zero,
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.5,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  children: options.map((option) => AmountCard(option)).toList(),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                cursorColor: AppColors.primaryLight,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Add Custom Amount',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey,width: 1)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey,width: 1)
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.grey,width: 1)
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 48,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                color: AppColors.primaryLight,
+                onPressed: () {  }, child: Text("Continue",style: TextStyle(color: AppColors.textWhite,fontSize: 16,fontWeight: FontWeight.w900),),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AmountOption {
+  final int amount;
+  final bool isPopular;
+  // final bool isCustom;
+
+  AmountOption(this.amount, {this.isPopular = false,/*this.isCustom =false*/});
+}
+
+class AmountCard extends StatelessWidget {
+  final AmountOption option;
+
+  const AmountCard(this.option, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          elevation: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  option.amount.toString(),
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (option.isPopular)
+          Positioned(
+            right: 0,
+            left: 0,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+              decoration:  BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12)
+                ),
+                // borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Text(
+                "★ Most Popular",
+                style: TextStyle(color: Colors.white, fontSize: 10),
+              ),
+            ),
+          )
+      ],
+    );
+  }
+}
--- lib/client/screens/astrologer_list_screen.dart
+++ lib/client/screens/astrologer_list_screen.dart
@@ -1,34 +1,65 @@
 import 'package:astrology_app/common/utils/app_text_styles.dart';
+import 'package:astrology_app/common/utils/colors.dart';
+import 'package:astrology_app/common/utils/constants.dart';
 import 'package:astrology_app/common/utils/images.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import '../model/user_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AstrologerListScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> astrologers = [
-    {
-      'name': 'Lorem ipsum',
-      'image': 'https://img.freepik.com/free-psd/spectacular-flight-vivid-green-bird_191095-78393.jpg',
-      'languages': 'English, Hindi',
-      'experience': '8 Years',
-      'rate': '30/min',
-      'waitTime': '5m',
-      'isOnline': true,
-      'chat': true,
-      'call': true,
-      'video': true,
-    },
-    {
-      'name': 'Lorem ipsum',
-      'image': 'https://img.freepik.com/free-psd/conquering-summit-watercolor-painting-hikers-triumph_191095-77950.jpg',
-      'languages': 'English, Hindi',
-      'experience': '8 Years',
-      'rate': '30/min',
-      'waitTime': '3m',
-      'isOnline': true,
-      'chat': true,
-      'call': true,
-      'video': false,
-    },
-  ];
+class AstrologerListScreen extends StatefulWidget {
+  @override
+  State<AstrologerListScreen> createState() => _AstrologerListScreenState();
+}
+
+class _AstrologerListScreenState extends State<AstrologerListScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  List<UserModel> _astrologersFuture=[];
+  int selectedSkill=-1;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAstrologers();
+  }
+
+  // final List<Map<String, dynamic>> astrologers = [
+   _fetchAstrologers({String? skill}) async {
+
+    try {
+      if(skill!=null){
+        final querySnapshot = await _firestore
+            .collection('users')
+            .where('astrologerProfile', isNotEqualTo: null)
+            .where('astrologerProfile.status', isEqualTo: 'approved')
+            .where('astrologerProfile.isOnline', isEqualTo: true)
+            .where('astrologerProfile.skills', arrayContains: '$skill')
+            .get();
+
+        _astrologersFuture= querySnapshot.docs
+            .map((doc) => UserModel.fromJson(doc.data()))
+            .toList();
+      }else{
+        final querySnapshot = await _firestore
+            .collection('users')
+            .where('astrologerProfile', isNotEqualTo: null)
+            .where('astrologerProfile.status', isEqualTo: 'approved')
+            .where('astrologerProfile.isOnline', isEqualTo: true)
+            .get();
+
+        _astrologersFuture= querySnapshot.docs
+            .map((doc) => UserModel.fromJson(doc.data()))
+            .toList();
+      }
+      setState(() {
+
+      });
+    } catch (e) {
+      debugPrint('Error fetching astrologers: $e');
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,11 +104,15 @@ class AstrologerListScreen extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 padding: EdgeInsets.symmetric(horizontal: 16),
                 children: [
-                  FilterChipWidget(label: "Filter"),
-                  FilterChipWidget(label: "All"),
-                  FilterChipWidget(label: "Love"),
-                  FilterChipWidget(label: "Offer"),
-                  FilterChipWidget(label: "Education"),
+                  FilterChipWidget(label: "All",isSelected:selectedSkill==-1,onTapCall:(){
+                    selectedSkill=-1;
+                    _fetchAstrologers();
+                  }),
+                  for(int i=0;i<AppConstants.astrologySkills.length;i++)
+                  FilterChipWidget(label: AppConstants.astrologySkills[i],isSelected:i==selectedSkill,onTapCall:(){
+                    selectedSkill=i;
+                    _fetchAstrologers(skill: AppConstants.astrologySkills[i]);
+                  }),
                 ],
               ),
             ),
@@ -85,9 +120,9 @@ class AstrologerListScreen extends StatelessWidget {
             // Astrologer List
             Expanded(
               child: ListView.builder(
-                itemCount: astrologers.length,
+                itemCount: _astrologersFuture.length,
                 itemBuilder: (context, index) {
-                  final astro = astrologers[index];
+                  final astro = _astrologersFuture[index];
                   return AstrologerCard(astro: astro);
                 },
               ),
@@ -101,24 +136,33 @@ class AstrologerListScreen extends StatelessWidget {
 
 class FilterChipWidget extends StatelessWidget {
   final String label;
-
-  const FilterChipWidget({required this.label});
+  bool? isSelected;
+  var onTapCall;
+  FilterChipWidget({required this.label,this.isSelected, required this.onTapCall});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 10),
-      child: Chip(
-        label: Text(label),
-        backgroundColor: Colors.purple.shade700,
-        labelStyle: TextStyle(color: Colors.white),
+      child: InkWell(
+        onTap: () {
+          onTapCall();
+        },
+        child: Chip(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24)
+          ),
+          label: Text(label),
+          backgroundColor: isSelected==true?AppColors.primaryLight:Colors.grey,
+          labelStyle: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
 }
 
 class AstrologerCard extends StatelessWidget {
-  final Map<String, dynamic> astro;
+  final UserModel astro;
 
   const AstrologerCard({required this.astro});
 
@@ -140,7 +184,7 @@ class AstrologerCard extends StatelessWidget {
                 ClipRRect(
                   borderRadius: BorderRadius.circular(8),
                   child: Image.network(
-                    astro['image'],
+                    astro.astrologerProfile!.imageUrl??'https://img.freepik.com/free-psd/spectacular-flight-vivid-green-bird_191095-78393.jpg',
                     width: 70,
                     height: 70,
                     fit: BoxFit.cover,
@@ -153,23 +197,36 @@ class AstrologerCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
                         children: [
-                          Text(astro['name'],
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16)),
-                          SizedBox(width: 6),
-                          Icon(Icons.verified, color: Colors.green, size: 16),
+                          Row(
+                            children: [
+                              Text(astro.name.toString(),
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16)),
+                              if(astro.astrologerProfile!.status==AstrologerStatus.approved)
+                              SizedBox(width: 6),
+                              if(astro.astrologerProfile!.status==AstrologerStatus.approved)
+                              Icon(Icons.verified, color: Colors.green, size: 16),
+                            ],
+                          ),
+                          Text(
+                            astro.isOnline==true? "Online" : "Offline",
+                            style: TextStyle(
+                                color: Colors.greenAccent, fontWeight: FontWeight.bold),
+                          ),
                         ],
                       ),
                       SizedBox(height: 4),
                       Row(
                         children: [
-                          Icon(Icons.location_city),
+                          Icon(Icons.menu_book_sharp,color:Colors.grey),
                           Expanded(
                             child: Text(
-                              'Vastu consultation, Vedic Astrology +7 more',
+                              astro.astrologerProfile!.skills!.join(','),
                               style: TextStyle(color: Colors.grey, fontSize: 12),
                             ),
                           ),
@@ -177,10 +234,10 @@ class AstrologerCard extends StatelessWidget {
                       ),
                       Row(
                         children: [
-                          Icon(Icons.language),
+                          Icon(Icons.language,color:Colors.grey),
                           Expanded(
                             child: Text(
-                              '${astro['languages']}',
+                              '${astro.languages!.join(',')}',
                               style: TextStyle(color: Colors.grey, fontSize: 12),
                             ),
                           ),
@@ -188,51 +245,64 @@ class AstrologerCard extends StatelessWidget {
                       ),
                       Row(
                         children: [
-                          Icon(Icons.cast_for_education),
+                          Icon(Icons.timelapse_sharp,color:Colors.grey),
                           Text(
-                            '${astro['experience']}',
+                            astro.astrologerProfile!.yearsOfExperience.toString(),
                             style: TextStyle(color: Colors.grey, fontSize: 12),
                           ),
                         ],
                       ),
                       SizedBox(height: 4),
-                      Text(
-                        '₹ ${astro['rate']}',
-                        style: TextStyle(color: Colors.white),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '₹ 10',
+                            style: TextStyle(color: Colors.white),
+                          ),
+
+                          Text(
+                            'Wait = 3m',
+                            style: TextStyle(color: Colors.redAccent, fontSize: 12),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
-                // Status
-                Padding(
-                  padding: const EdgeInsets.only(left: 4.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        astro['isOnline'] ? "Online" : "Offline",
-                        style: TextStyle(
-                            color: Colors.greenAccent, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        'Wait = ${astro['waitTime']}',
-                        style: TextStyle(color: Colors.redAccent, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                )
+                // // Status
+                // Padding(
+                //   padding: const EdgeInsets.only(left: 4.0),
+                //   child: Column(
+                //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //     mainAxisSize: MainAxisSize.max,
+                //     crossAxisAlignment: CrossAxisAlignment.end,
+                //     children: [
+                //       Text(
+                //         astro['isOnline'] ? "Online" : "Offline",
+                //         style: TextStyle(
+                //             color: Colors.greenAccent, fontWeight: FontWeight.bold),
+                //       ),
+                //       SizedBox(height: 4),
+                //       Text(
+                //         'Wait = ${astro['waitTime']}',
+                //         style: TextStyle(color: Colors.redAccent, fontSize: 12),
+                //       ),
+                //     ],
+                //   ),
+                // )
               ],
             ),
             Divider(color: Colors.grey[700], height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                _buildActionButton(Icons.chat, "Chat", astro['chat']),
-                _buildActionButton(Icons.call, "Call", astro['call']),
-                _buildActionButton(Icons.video_call, "Video Call", astro['video']),
+                _buildActionButton(Icons.chat, "Chat",),
+                _buildActionButton(Icons.call, "Call",onClick:(){
+                  launchUrl(Uri.parse('tel:${astro.phoneNumber}'));
+                }),
+                _buildActionButton(Icons.video_call, "Video Call", ),
               ],
             )
           ],
@@ -241,18 +311,20 @@ class AstrologerCard extends StatelessWidget {
     );
   }
 
-  Widget _buildActionButton(IconData icon, String label, bool enabled) {
+  Widget _buildActionButton(IconData icon, String label, {var onClick}) {
     return ElevatedButton.icon(
-      onPressed: enabled ? () {} : null,
-      icon: Icon(icon, size: 18),
-      label: Text(label),
+      onPressed:  () {
+        onClick();
+      } ,
+      icon: Icon(icon, size: 18,color:Colors.green),
+      label: Text(label,style: TextStyle(color: Colors.green),),
       style: ElevatedButton.styleFrom(
-        backgroundColor: enabled ? Colors.green : Colors.grey[800],
-        foregroundColor: Colors.white,
+        backgroundColor:Colors.grey[800],
+        foregroundColor:Colors.white,
         disabledForegroundColor: Colors.white54,
         disabledBackgroundColor: Colors.grey[800],
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-        textStyle: TextStyle(fontSize: 12),
+        textStyle: TextStyle(fontSize: 12,),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
